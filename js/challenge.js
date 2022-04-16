de0ffa5993cd2de49bbf6b9108ac7bf57be08be5
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-16 15:22:06 +0300

    Timer

---
+++ js/challenge.js
@@ -0,0 +1,63 @@
+let counter = document.getElementById('counter')
+let counterCont = Number(counter.textContent)
+console.log(counterCont);
+
+function timed(){
+    counterCont += 1;
+    counter.innerHTML = counterCont;
+}
+let start = setInterval( timed,1000
+)
+
+
+document.getElementById('minus').addEventListener('click', ()=>{
+    counter.innerHTML = counterCont --
+    console.log(counterCont);
+})
+
+document.getElementById('plus').addEventListener('click', () =>{
+    counter.innerHTML = counterCont ++
+})
+
+document.getElementById('heart').addEventListener('click', () =>{
+    let likes = document.createElement('li')
+    likes.innerHTML = `${counterCont} has been liked 1 time`
+    document.querySelector('.likes').appendChild(likes)
+})
+
+let pauseTimer = document.getElementById('pause')
+pauseTimer.addEventListener('click',pauseResume)
+function pauseResume(){
+    if(pauseTimer.textContent === "pause"){
+       
+        pauseTimer.textContent = "resume"
+        document.getElementById('submit').disabled = true;
+        document.getElementById('minus').disabled = true;
+        document.getElementById('plus').disabled = true;
+        document.getElementById('heart').disabled = true;
+        clearInterval(start)
+  
+    }else{
+        pauseTimer.textContent = "pause"
+        document.getElementById('submit').disabled = false;
+        document.getElementById('minus').disabled = false;
+        document.getElementById('plus').disabled = false;
+        document.getElementById('heart').disabled = false;
+
+        start = setInterval(timed,1000)
+       
+    }
+}
+    
+
+let form = document.querySelector('form')
+console.log(form);
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let comment = form.querySelector('#comment-input').value 
+    console.log(comment);
+    let newCom = document.createElement('p')
+    newCom.textContent = comment
+   document.querySelector('#list').appendChild(newCom)
+   e.target.reset();
+})
