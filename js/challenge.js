b5115917f7e8e17852378fd0b7f4201d834c3308
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-19 10:56:14 +0300

    Add pause and resume functionality

--- js/challenge.js
+++ js/challenge.js
@@ -6,9 +6,7 @@ function timed(){
     counterCont += 1;
     counter.innerHTML = counterCont;
 }
-let start = setInterval( timed,1000
-)
-
+let start = setInterval( timed,1000)
 
 document.getElementById('minus').addEventListener('click', ()=>{
     counter.innerHTML = counterCont --
@@ -26,25 +24,27 @@ document.getElementById('heart').addEventListener('click', () =>{
 })
 
 let pauseTimer = document.getElementById('pause')
-pauseTimer.addEventListener('click',pauseResume)
+pauseTimer.addEventListener('click', pauseResume)
 function pauseResume(){
     if(pauseTimer.textContent === "pause"){
-       
-        pauseTimer.textContent = "resume"
+        console.log("Working")
+        clearInterval(start)
+        pauseTimer.innerText = "resume"
+
         document.getElementById('submit').disabled = true;
         document.getElementById('minus').disabled = true;
         document.getElementById('plus').disabled = true;
         document.getElementById('heart').disabled = true;
-        clearInterval(start)
   
-    }else{
+    }else if(pauseTimer.innerText === 'resume'){
+        console.log('Sheesh')
+        start = setInterval(timed,1000)
+        
         pauseTimer.textContent = "pause"
         document.getElementById('submit').disabled = false;
         document.getElementById('minus').disabled = false;
         document.getElementById('plus').disabled = false;
         document.getElementById('heart').disabled = false;
-
-        start = setInterval(timed,1000)
        
     }
 }
